02c573db32e87ad752fbaa70c855d2eb314cd2d6
claudiob <claudiob@gmail.com>
2013-09-02 11:09:45 -0700

    Another use: create

--- spec/support/instances_helper.rb
+++ spec/support/instances_helper.rb
@@ -2,8 +2,8 @@ require 'spec_helper'
 require 'rspec_api_documentation/dsl'
 
 def setup_instances
-  Concert.create where: 'Coachella', year: 2013
-  Concert.create where: 'Woodstock', year: 1969
+  instances.create where: 'Coachella', year: 2013
+  instances.create where: 'Woodstock', year: 1969
 end
 
 def instances
