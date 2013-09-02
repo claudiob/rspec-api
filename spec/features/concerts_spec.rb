8589eaed7d211c1ff3692cbd4c883b1aa6fa1a17
claudiob <claudiob@gmail.com>
2013-09-01 22:54:06 -0700

    Better doc titles

--- spec/features/concerts_spec.rb
+++ spec/features/concerts_spec.rb
@@ -8,7 +8,7 @@ resource 'Concerts', accepts: :json, returns: :json do
   end
 
   get '/concerts' do
-    example_request 'lists all concerts' do
+    example_request 'Get the list of concerts' do
       respond_with 200 do |concerts|
         expect(concerts).to eq [{"where"=>"Coachella", "year"=>2013}, {"where"=>"Woodstock", "year"=>1969}]
       end
@@ -16,25 +16,25 @@ resource 'Concerts', accepts: :json, returns: :json do
   end
 
   get '/concerts/:id' do
-    example_request 'shows a concert given an existing ID', id: 2 do
+    example_request 'Get an existing concert', id: 2 do
       respond_with 200 do |concert|
         expect(concert).to eq "where"=>"Woodstock", "year"=>1969
       end
     end
 
-    example_request 'returns an error when showing a concert with an unknown ID', id: 3 do
+    example_request 'Get an unknown concert', id: 3 do
       respond_with 404
     end
   end
 
   post '/concerts' do
-    example_request 'creates a concert given valid data', concert: {where: 'Austin'} do
+    example_request 'Create a valid concert', concert: {where: 'Austin'} do
       respond_with 201 do |concert|
         expect(concert).to eq "where"=>"Austin", "year"=>nil
       end
     end
 
-    example_request 'returns an error when creating a concert with invalid data', concert: {year: 2013} do
+    example_request 'Create an invalid concert', concert: {year: 2013} do
       respond_with 422 do |errors|
         expect(errors).to eq "where"=>["can't be blank"]
       end
@@ -42,23 +42,23 @@ resource 'Concerts', accepts: :json, returns: :json do
   end
 
   put '/concerts/:id' do
-    example_request 'updates a concert given an existing ID', id: 1, concert: {year: 2011} do
+    example_request 'Update an existing concert', id: 1, concert: {year: 2011} do
       respond_with 200 do |concert|
         expect(concert).to eq "where"=>"Coachella", "year"=>2011
       end
     end
 
-    example_request 'returns an error when updating a concert with an unknown ID', id: 3, concert: {year: 2011} do
+    example_request 'Update an unknown concert', id: 3, concert: {year: 2011} do
       respond_with 404
     end
   end
 
   delete '/concerts/:id' do
-    example_request 'deletes a concert given an existing ID', id: 1 do
+    example_request 'Delete an existing concert', id: 1 do
       respond_with 204
     end
 
-    example_request 'returns an error when deleting a concert with an unknown ID', id: 3 do
+    example_request 'Delete an unknown concert', id: 3 do
       respond_with 404
     end
   end
