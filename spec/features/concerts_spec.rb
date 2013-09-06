4683b80eb12a0b7629cb51b2af8137751691ade4
claudiob <claudiob@gmail.com>
2013-09-06 01:01:25 -0700

    Use shorter version with, with valid, with no_params and achieve 70 lines of code

--- spec/features/concerts_spec.rb
+++ spec/features/concerts_spec.rb
@@ -17,14 +17,13 @@ resource 'Concerts', accepts: :json, returns: :json do
 
   accepts page: :page
 
+
   get '/concerts', array: true do
-    request 'Get the list of concerts' do
-      respond_with :ok
-    end
+    with no_params do respond_with :ok end
   end
 
   get '/locations/:location/concerts', array: true do
-    request 'Get the concerts in a location', location: apply(:downcase, to: existing(:where)) do
+    with location: apply(:downcase, to: existing(:where)) do
       respond_with :ok do |concerts|
         concerts.each do |concert|
           expect(concert['where'].downcase).to eq request_params[:location]
@@ -34,23 +33,18 @@ resource 'Concerts', accepts: :json, returns: :json do
   end
 
   get '/concerts/:id' do
-    request 'Get an existing concert', id: existing(:id) do
-      respond_with :ok
-    end
-
-    request 'Get an unknown concert', id: unknown(:id) do
-      respond_with :not_found
-    end
+    with id: existing(:id) do respond_with :ok end
+    with id: unknown(:id) do respond_with :not_found end
   end
 
   post '/concerts' do
-    request 'Create a valid concert', concert: {where: 'Austin'} do
+    with valid concert: {where: 'Austin'} do
       respond_with :created do |concert|
         expect(concert['where']).to eq 'Austin'
       end
     end
 
-    request 'Create an invalid concert', concert: {year: 2013} do
+    with invalid concert: {year: 2013} do
       respond_with :unprocessable_entity do |errors|
         expect(errors["where"]).to eq ["can't be blank"]
       end
@@ -58,24 +52,19 @@ resource 'Concerts', accepts: :json, returns: :json do
   end
 
   put '/concerts/:id' do
-    request 'Update an existing concert', id: existing(:id), concert: {year: 2011} do
+    with id: existing(:id), concert: {year: 2011} do
       respond_with :ok do |concert|
         expect(concert["year"]).to be 2011
       end
     end
 
-    request 'Update an unknown concert', id: unknown(:id), concert: {year: 2011} do
+    with id: unknown(:id), concert: {year: 2011} do
       respond_with :not_found
     end
   end
 
   delete '/concerts/:id' do
-    request 'Delete an existing concert', id: existing(:id) do
-      respond_with :no_content
-    end
-
-    request 'Delete an unknown concert', id: unknown(:id) do
-      respond_with :not_found
-    end
+    with id: existing(:id) do respond_with :no_content end
+    with id: unknown(:id) do respond_with :not_found end
   end
 end
--- spec/support/description_helper.rb
+++ spec/support/description_helper.rb
@@ -1,14 +1,57 @@
 require 'spec_helper'
 
 def existing(key)
+  metadata[:description_attribute] = 'an existing'
   -> { instances.any key }
 end
 
 def unknown(key)
+  metadata[:description_attribute] = 'an unknown'
   -> { instances.none key }
 end
 
 def apply(method_name, options = {})
   proc = options[:to]
   -> { proc.call.send method_name }
+end
+
+def with(request_params = {})
+  request_params[:attribute] ||= metadata.delete :description_attribute
+  request description_for(request_params), request_params, &Proc.new
+end
+
+def no_params
+  {}
+end
+
+def valid(request_params)
+  request_params.merge attribute: 'an valid'
+end
+
+def invalid(request_params)
+  request_params.merge attribute: 'an invalid'
+end
+
+def description_for(request_params = {})
+  [description_verb, description_object(request_params)].join ' '
+end
+
+def description_verb
+  case metadata[:method]
+  when :get then 'Getting'
+  when :post then 'Creating'
+  when :put then 'Updating'
+  when :delete then 'Deleting'
+  end
+end
+
+def description_object(request_params = {})
+  attribute = request_params.delete :attribute
+  if metadata[:array]
+    "a list of #{metadata[:resource_name]}".tap do |objects|
+      objects << " by #{request_params.keys.join(', ')}" if request_params.any?
+    end
+  else
+    [attribute, metadata[:resource_name].singularize].join ' '
+  end.downcase
 end
